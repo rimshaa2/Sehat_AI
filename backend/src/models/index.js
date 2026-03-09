e7ca9d27be608248a06379c7b7bcc044e80c977a
Rimsha <rimshasajid45@gmail.com>
2026-03-09 19:39:51 +0500

    fixed medical records issue

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -5,7 +5,7 @@ const User = require('./User');
 const Doctor = require('./Doctor');
 const Appointment = require('./Appointment');
 const Availability = require('./Availability'); 
-const MedicalRecord = require('./MedicalRecord');
+const MedicalRecord = require('./medicalRecord');
 
 // 2. Define Relationships
 
